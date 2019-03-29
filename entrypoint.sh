4b380a4fd3d1de76c2d32329ffdaed3a9e7e0229
tcitry <tcitry@gmail.com>
2019-03-29 22:00:57 +0800

    fix hugo version

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,12 +12,22 @@ fi
 if [ -z "${TARGET_REPO}" ]; then
   TARGET_REPO=${GITHUB_REPOSITORY}
 fi
-
 REMOTE_REPO="https://${TOKEN}@github.com/${TARGET_REPO}.git"
+
+if [ -z "$HUGO_VERSION" ]; then
+    HUGO_VERSION=0.54.0
+    echo 'No HUGO_VERSION was set, so defaulting to '$HUGO_VERSION
+fi
+
 cd "${GITHUB_WORKSPACE}" || exit 1
+
+echo 'installing hugo'
+curl -sSL https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz > hugo.tar.gz && tar -f hugo.tar.gz -xz
+
 git submodule update --init --recursive
 rm -rf .git
-hugo -v
+echo "hugo version is" && ./hugo version
+./hugo
 cd public
 git init
 git config user.name "${GITHUB_ACTOR}"
